7c1b8c7fa3f1db67e54a6323bcb01345b0ec4a44
Damien Lebrun <dinoboff@hotmail.com>
2016-08-14 14:54:13 +0100

    Fix initial timestamp
    The server timestamps of the data (read) or newData (write/patch) snapshot do not necessary match the rulesets “now” timestamp.
    
    The tests are unstable, relying on the snapshot and ruleset state `now` variable being created in the same nanosecond.
    
    This fix attaches the timestamp to the snapshot and uses it to evaluate the ruleset.
    
    API changes (backward compatible):
    - `targaryen/lib/rule-data-snapshot.RuleDataSnapshot(data[, now], path)`;
    - `targaryen/lib/rule-data-snapshot.RuleDataSnapshot.convert(data[, now])`;
    - `targaryen/lib/helpers.makeNewDataSnap(path, data[, now])`;
    - `targaryen.setFirebaseData(data[, now])`;
    - `targayen/lib/ruleset.prototype.tryRead(path, root, auth[, now])`;
    - `targayen/lib/ruleset.prototype.tryWrite(path, root, newData, auth[, skipWrite[, skipValidate[, skipOnNoValue[, now]]]])`;
    - `targayen/lib/ruleset.prototype.tryPatch(path, root, newData, auth[, skipWrite[, skipValidate[, skipOnNoValue[, now]]]])`;
    - jasmine matchers gain an optional “now” argument;
    - chai `write` and `patch` methods gain an optional “now” argument.
    
    `now` default to the value returned by`Date.now()` if not provided.
    
    Fix #29.

--- test/spec/lib/helpers.js
+++ test/spec/lib/helpers.js
@@ -38,31 +38,38 @@ describe('helpers', function() {
     it('should create a snapshot for the path', function() {
       var snapshot = helpers.makeNewDataSnap('foo/bar/baz', 1);
 
-      expect(snapshot).to.eql(new RuleDataSnapshot({
+      expect(snapshot.val()).to.eql({
         foo: {
           bar: {
-            baz: {
-              '.value': 1,
-              '.priority': null
-            }
+            baz: 1
           }
         }
-      }));
+      });
     });
 
     it('should trim the begining of the path', function() {
       var snapshot = helpers.makeNewDataSnap('/foo/bar/baz', 1);
 
-      expect(snapshot).to.eql(new RuleDataSnapshot({
+      expect(snapshot.val()).to.eql({
         foo: {
           bar: {
-            baz: {
-              '.value': 1,
-              '.priority': null
-            }
+            baz: 1
           }
         }
-      }));
+      });
+    });
+
+    it('should convert timestamp server values', function() {
+      var now = 12345000,
+        snapshot = helpers.makeNewDataSnap('foo/bar/baz', {'.sv': 'timestamp'}, now);
+
+      expect(snapshot.val()).to.eql({
+        foo: {
+          bar: {
+            baz: now
+          }
+        }
+      });
     });
 
   });
