c5d64f61247ffd21d11c8cea2b5dc8b4c41f3cd3
Damien Lebrun <dinoboff@hotmail.com>
2016-11-02 11:29:05 +0100

    Disable direct access to plugins or plugin helpers

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 
 'use strict';
 
-var helpers = require('./lib/test-helpers');
+const helpers = require('./util');
+const util = require('util');
 
-module.exports = {
-  setFirebaseData: helpers.setFirebaseData,
-  setFirebaseRules: helpers.setFirebaseRules,
-  setDebug: helpers.setDebug,
-  users: helpers.users,
-  utils: helpers,
-  chai: require('./lib/chai'),
-  jasmine: require('./lib/jasmine'),
-};
+exports.util = helpers;
+
+// Deprecate direct access to plugin helpers
+Object.defineProperties(exports, [
+  'setFirebaseData',
+  'setFirebaseRules',
+  'setDebug',
+  'users'
+].reduce((props, key) => {
+  props[key] = {
+    get: util.deprecate(
+      () => helpers[key],
+      `Deprecated: use "chai.${key}" or "jasmine.${key}" directly.`
+    ),
+    enumerable: true,
+    configurable: true
+  };
+
+  return props;
+}, {}));
+
+// Deprecate direct access to plugins
+Object.defineProperties(exports, [
+  'chai',
+  'jasmine'
+].reduce((props, key) => {
+  const path = `./plugins/${key}`;
+
+  props[key] = {
+    get: util.deprecate(
+      () => require(path),
+      `Deprecated: use "const ${key} = require('targaryen/plugins/${key}');"`
+    ),
+    enumerable: true,
+    configurable: true
+  };
+
+  return props;
+}, {}));
