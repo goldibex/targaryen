917f37d12f0f6a9bd3244bf5af1811226c4f1173
Damien Lebrun <dinoboff@hotmail.com>
2016-11-05 15:48:02 +0100

    renamed lib/helpers to lib/paths

---
+++ test/spec/lib/paths.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const paths = require('../../../lib/paths.js');
+
+describe('paths', function() {
+
+  describe('pathMerger', function() {
+
+    it('should merge path', function() {
+      expect(paths.pathMerger('foo', 'bar/baz')).to.equal('foo/bar/baz');
+      expect(paths.pathMerger('foo', '')).to.equal('foo');
+      expect(paths.pathMerger('', 'bar/baz')).to.equal('bar/baz');
+    });
+
+    it('should trim root path', function() {
+      expect(paths.pathMerger('/foo/', 'bar/baz')).to.equal('foo/bar/baz');
+    });
+
+    it('should trim the beginning of the path', function() {
+      expect(paths.pathMerger('foo', '/bar/baz')).to.equal('foo/bar/baz');
+    });
+
+  });
+
+  describe('pathSplitter', function() {
+
+    it('should split the path', function() {
+      expect(paths.pathSplitter('foo/bar/baz')).to.eql(['foo', 'bar', 'baz']);
+    });
+
+    it('should no split empty path', function() {
+      expect(paths.pathSplitter('')).to.eql([]);
+    });
+
+
+    it('should trim the beginning of the path', function() {
+      expect(paths.pathSplitter('/foo/bar/baz')).to.eql(['foo', 'bar', 'baz']);
+    });
+
+  });
+
+});
