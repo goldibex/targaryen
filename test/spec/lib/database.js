24cd150d2248e3ec28e2b7c047227db115846add
Damien Lebrun <dinoboff@hotmail.com>
2016-12-07 16:35:33 +0000

    Simpler rule and data for each snapshot tests

--- test/spec/lib/database.js
+++ test/spec/lib/database.js
@@ -41,33 +41,10 @@ describe('database', function() {
   });
 
   describe('snapshot', function() {
-    let root;
+    let rules;
 
     beforeEach(function() {
-      root = database.snapshot('/', {
-        '.priority': 'hello',
-        users: {
-          'password:c7ec6752-45b3-404f-a2b9-7df07b78d28e': {
-            '.priority': 1,
-            name: {'.value': 'Sherlock Holmes'},
-            genius: {'.value': true},
-            arrests: {'.value': 70}
-          },
-          'password:500f6e96-92c6-4f60-ad5d-207253aee4d3': {
-            '.priority': 2,
-            name: {'.value': 'John Watson'}
-          },
-          'password:3403291b-fdc9-4995-9a54-9656241c835d': {
-            '.priority': 0,
-            name: {'.value': 'Inspector Lestrade'},
-            arrests: {'.value': 35}
-          },
-          'password:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx': {
-            '.priority': 0,
-            '.value': null
-          }
-        }
-      });
+      rules = {rules: {}};
     });
 
     describe('create', function() {
@@ -87,23 +64,42 @@ describe('database', function() {
     describe('#val', function() {
 
       it('gets the value at the specified path', function() {
+        const data = {
+          foo: {
+            bar: 1,
+            baz: 2
+          },
+          qux: 3
+        };
+        const db = database.create(rules, data);
 
-        expect(root.val()).to.deep.equal({
-          users: {
-            'password:c7ec6752-45b3-404f-a2b9-7df07b78d28e': {name: 'Sherlock Holmes', genius: true, arrests: 70},
-            'password:500f6e96-92c6-4f60-ad5d-207253aee4d3': {name: 'John Watson'},
-            'password:3403291b-fdc9-4995-9a54-9656241c835d': {name: 'Inspector Lestrade', arrests: 35}
-          }
-        });
-
+        expect(db.snapshot('/').val()).to.deep.equal(data);
+        expect(db.snapshot('/foo').val()).to.deep.equal(data.foo);
+        expect(db.snapshot('/foo/bar').val()).to.deep.equal(data.foo.bar);
+        expect(db.snapshot('/foo/baz').val()).to.deep.equal(data.foo.baz);
+        expect(db.snapshot('/qux').val()).to.deep.equal(data.qux);
       });
 
     });
 
     describe('#getPriority', function() {
 
-      it('gets the priority at the specified path', function() {
-        expect(root.getPriority()).to.equal('hello');
+      it.skip('gets the priority at the specified path', function() {
+        const data = {
+          '.priority': 1,
+          foo: {
+            bar: {
+              '.priority': 10,
+              '.value': 1
+            },
+            baz: 2
+          }
+        };
+        const db = database.create(rules, data);
+
+        expect(db.snapshot('/').getPriority()).to.equal(1);
+        expect(db.snapshot('/foo/bar').getPriority()).to.equal(10);
+        expect(db.snapshot('/foo/baz').getPriority()).to.be.null();
       });
 
     });
@@ -111,12 +107,16 @@ describe('database', function() {
     describe('#child', function() {
 
       it('gets a new data snapshot for the specified child key', function() {
-        expect(
-          root
-            .child('users/password:500f6e96-92c6-4f60-ad5d-207253aee4d3')
-            .child('name')
-            .val()
-        ).to.equal('John Watson');
+        const data = {
+          foo: {
+            bar: 1,
+            baz: 2
+          },
+          qux: 3
+        };
+        const snap = database.create(rules, data).snapshot('/');
+
+        expect(snap.child('foo').val()).to.deep.equal(data.foo);
       });
 
     });
@@ -124,18 +124,20 @@ describe('database', function() {
     describe('#parent', function() {
 
       it('gets the parent of the snap', function() {
+        const foo = {
+          bar: 1,
+          baz: 2
+        };
+        const snap = database.create(rules, {foo}).snapshot('/foo');
 
-        expect(
-          root
-            .child('users/password:500f6e96-92c6-4f60-ad5d-207253aee4d3/name')
-            .parent()
-            .val()
-        ).to.deep.equal({name: 'John Watson'});
-
+        expect(snap.parent().val()).to.deep.equal({foo});
       });
 
-      it('returns null if we are at the top', function() {
-        expect(root.parent()).to.be.null();
+      it.skip('fail if the snapshot was refering to the data root ', function() {
+        const data = null;
+        const snap = database.create(rules, data).snapshot('/');
+
+        expect(() => snap.parent()).to.throw();
       });
 
     });
@@ -143,11 +145,19 @@ describe('database', function() {
     describe('#exists', function() {
 
       it('returns true if some data is at that key', function() {
-        expect(root.child('users').exists()).to.be.true();
+        const data = {foo: 1};
+        const snap = database.create(rules, data).snapshot('/');
+
+        expect(snap.exists()).to.be.true();
+        expect(snap.child('foo').exists()).to.be.true();
       });
 
-      it('returns false if no data is at that key', function() {
-        expect(root.child('nonexistent').exists()).to.be.false();
+      it('returns true if some data is at that key', function() {
+        const data = null;
+        const snap = database.create(rules, data).snapshot('/');
+
+        expect(snap.exists()).to.be.false();
+        expect(snap.child('foo').exists()).to.be.false();
       });
 
     });
@@ -155,11 +165,17 @@ describe('database', function() {
     describe('#hasChild', function() {
 
       it('returns true if the path has a child with the given name', function() {
-        expect(root.hasChild('users')).to.be.true();
+        const data = {foo: 1};
+        const snap = database.create(rules, data).snapshot('/');
+
+        expect(snap.hasChild('foo')).to.be.true();
       });
 
       it('returns false if the path does not have a child with the given name', function() {
-        expect(root.hasChild('nonexistent')).to.be.false();
+        const data = {foo: 1};
+        const snap = database.create(rules, data).snapshot('/');
+
+        expect(snap.hasChild('bar')).to.be.false();
       });
 
     });
@@ -169,19 +185,17 @@ describe('database', function() {
       describe('with no arguments', function() {
 
         it('returns true if the path has any children at all', function() {
-          expect(
-            root
-              .child('users/password:500f6e96-92c6-4f60-ad5d-207253aee4d3')
-              .hasChildren()
-          ).to.be.true();
+          const data = {foo: 1};
+          const snap = database.create(rules, data).snapshot('/');
+
+          expect(snap.hasChildren()).to.be.true();
         });
 
         it('returns false if the path has no children', function() {
-          expect(
-            root
-              .child('users/password:500f6e96-92c6-4f60-ad5d-207253aee4d3/name')
-              .hasChildren()
-          ).to.be.false();
+          const data = null;
+          const snap = database.create(rules, data).snapshot('/');
+
+          expect(snap.hasChildren()).to.be.false();
         });
 
       });
@@ -189,7 +203,10 @@ describe('database', function() {
       describe('with an empty array', function() {
 
         it('should should throw', function() {
-          expect(() => root.hasChildren([])).to.throw();
+          const data = {foo: 1};
+          const snap = database.create(rules, data).snapshot('/');
+
+          expect(() => snap.hasChildren([])).to.throw();
         });
 
       });
@@ -197,21 +214,17 @@ describe('database', function() {
       describe('with an array of child names', function() {
 
         it('returns true if the path has all the specified children', function() {
+          const data = {foo: 1, bar: 2};
+          const snap = database.create(rules, data).snapshot('/');
 
-          expect(
-            root
-              .child('users/password:c7ec6752-45b3-404f-a2b9-7df07b78d28e')
-              .hasChildren(['name', 'genius', 'arrests'])
-          ).to.be.true();
-
+          expect(snap.hasChildren(['foo', 'bar'])).to.be.true();
         });
 
         it('returns false if the path is missing even one of the specified children', function() {
-          expect(
-            root
-              .child('users/password:3403291b-fdc9-4995-9a54-9656241c835d')
-              .hasChildren(['name', 'genius', 'arrests'])
-          ).to.be.false();
+          const data = {foo: 1};
+          const snap = database.create(rules, data).snapshot('/');
+
+          expect(snap.hasChildren(['foo', 'bar'])).to.be.false();
         });
 
       });
@@ -221,19 +234,19 @@ describe('database', function() {
     describe('#isNumber', function() {
 
       it('returns true if the value at the path has type number', function() {
-        expect(
-          root
-            .child('users/password:3403291b-fdc9-4995-9a54-9656241c835d/arrests')
-            .isNumber()
-        ).to.be.true();
+        const data = 1;
+        const snap = database.create(rules, data).snapshot('/');
+
+        expect(snap.isNumber()).to.be.true();
       });
 
       it('returns false if the value at the path does not have type number', function() {
-        expect(
-          root
-            .child('users/password:500f6e96-92c6-4f60-ad5d-207253aee4d3/arrests')
-            .isNumber()
-        ).to.be.false();
+        const data = {foo: 'bar', bar: false};
+        const db = database.create(rules, data);
+
+        expect(db.snapshot('/').isNumber()).to.be.false();
+        expect(db.snapshot('/foo').isNumber()).to.be.false();
+        expect(db.snapshot('/bar').isNumber()).to.be.false();
       });
 
     });
@@ -241,19 +254,19 @@ describe('database', function() {
     describe('#isBoolean', function() {
 
       it('returns true if the value at the path has type boolean', function() {
-        expect(
-          root
-            .child('users/password:c7ec6752-45b3-404f-a2b9-7df07b78d28e/genius')
-            .isBoolean()
-        ).to.be.true();
+        const data = false;
+        const snap = database.create(rules, data).snapshot('/');
+
+        expect(snap.isBoolean()).to.be.true();
       });
 
       it('returns false if the value at the path does not have type boolean', function() {
-        expect(
-          root
-            .child('users/password:3403291b-fdc9-4995-9a54-9656241c835d/name')
-            .isBoolean()
-        ).to.be.false();
+        const data = {foo: 'bar', bar: 1};
+        const db = database.create(rules, data);
+
+        expect(db.snapshot('/').isBoolean()).to.be.false();
+        expect(db.snapshot('/foo').isBoolean()).to.be.false();
+        expect(db.snapshot('/bar').isBoolean()).to.be.false();
       });
 
     });
@@ -261,19 +274,19 @@ describe('database', function() {
     describe('#isString', function() {
 
       it('returns true if the value at the path has type string', function() {
-        expect(
-          root
-            .child('users/password:3403291b-fdc9-4995-9a54-9656241c835d/name')
-            .isString()
-        ).to.be.true();
+        const data = 'foo';
+        const snap = database.create(rules, data).snapshot('/');
+
+        expect(snap.isString()).to.be.true();
       });
 
       it('returns false if the value at the path does not have type string', function() {
-        expect(
-          root
-            .child('users/password:3403291b-fdc9-4995-9a54-9656241c835d')
-            .isString()
-        ).to.be.false();
+        const data = {foo: 1, bar: true};
+        const db = database.create(rules, data);
+
+        expect(db.snapshot('/').isString()).to.be.false();
+        expect(db.snapshot('/foo').isString()).to.be.false();
+        expect(db.snapshot('/bar').isString()).to.be.false();
       });
 
     });
@@ -281,11 +294,12 @@ describe('database', function() {
     describe('toString', function() {
 
       it('should return the snapshot path', function() {
-        expect(
-          root
-            .child('users/password:3403291b-fdc9-4995-9a54-9656241c835d/name')
-            .toString()
-        ).to.equal('users/password:3403291b-fdc9-4995-9a54-9656241c835d/name');
+        const data = null;
+        const db = database.create(rules, data);
+
+        expect(db.snapshot('/').toString()).to.equal('');
+        expect(db.snapshot('/foo').toString()).to.equal('foo');
+        expect(db.snapshot('/foo/bar').toString()).to.equal('foo/bar');
       });
 
     });
