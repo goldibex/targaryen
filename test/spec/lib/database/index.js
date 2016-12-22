ee9becffadccc65828ce4a6ba4aa573b64c4f143
Damien Lebrun <dinoboff@hotmail.com>
2016-12-22 16:52:48 +0000

    Remove reference to “.patch” rule in logs.

--- test/spec/lib/database/index.js
+++ test/spec/lib/database/index.js
@@ -1109,6 +1109,27 @@ describe('database', function() {
 
     });
 
+    it('should show .write rules did no allow the operation', function() {
+      const rules = {
+        rules: {
+          foo: {
+            '.write': true
+          }
+        }
+      };
+      const data = {};
+      const db = database.create(rules, data).with({debug: true});
+      const patch = {
+        foo: true,
+        bar: true
+      };
+
+      const result = db.update('/', patch);
+
+      expect(result.info).to.contain('No .write rule allowed the operation.');
+      expect(result.info).to.contain('patch was denied.');
+    });
+
   });
 
 });
